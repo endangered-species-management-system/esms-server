120298a65d5cf9294fe420f077c622a2098228ef
Todd A Nordquist <toddnordquist144@gmail.com>
2020-09-08 21:05:03 -0600

    Initial commit, added entity classes

---
+++ src/main/java/edu/cnm/deepdive/esms/model/entity/Researcher.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.esms.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Researcher {
+
+  @Id
+  private long Id;
+
+}
---
+++ src/test/java/edu/cnm/deepdive/esms/EsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.esms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
